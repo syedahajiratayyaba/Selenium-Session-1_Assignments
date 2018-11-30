a792d9890aab162e9b878fcbb37afa1bd6ed514d
Syeda Hajira Tayyaba <45248882+syedahajiratayyaba@users.noreply.github.com>
2018-11-30 14:45:34 +0530

    Add files via upload

---
+++ InternetExplorerLaunch.java
@@ -0,0 +1,16 @@
+package com.ibm.seleniumconcepts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class SeleniumBrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver=new InternetExplorerDriver();
+		System.out.println("Hello");
+	}
+
+}
